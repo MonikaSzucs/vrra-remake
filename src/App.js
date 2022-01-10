d058753b7bb8338b658a58bcd1cd3bc610a570bc
Monika <monika.silvia.s@hotmail.ca>
2022-01-10 13:23:10 -0800

    added test line

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <span>NEW LINE</span>
         <a
           className="App-link"
           href="https://reactjs.org"
