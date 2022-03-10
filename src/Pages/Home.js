5e15b9da184681f2b73ca87d04d77196a63afe37
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-03-09 18:18:09 -0800

    Setup banner, grid system, and cards for images

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,59 @@
 import React from 'react';
-import {
-    Button,
-    Container
-} from 'react-bootstrap';
+import { Typography } from '@mui/material';
+import Grid from '@material-ui/core/Grid';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import BackgroundHeader from "../Assets/Images/HomeBanner.jpg";
+import './Home.css';
+
+const styles = {
+    BackgroundHeader: {
+        backgroundImage: `url(${BackgroundHeader})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '50vh',
+    }
+};
 
 export default function Home() {
     return (
         <div>
-            HOME PAGE
+            <div style={styles.BackgroundHeader}>
+                <Grid container>
+                    <Grid item xs={0} md={1}></Grid>
+                    <Grid item xs={12} md={10}>
+                        <Typography component="div" style={{ color: "white" }}>
+                            <Box sx={{ fontSize: 'h3.fontSize', pt: 18, pb: 1  }} style={{  }}>Vancouver Rabbit Rescue Association</Box>
+                            <Box sx={{ fontSize: 'h6.fontSize', }}>A Chapter of the House Rabbit Society</Box>
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={0} md={1}></Grid>
+                </Grid>
+            </div>
+            <div>
+                <Grid container>
+                    <Grid item xs={0} md={1}></Grid>
+                    <Grid item xs={12} md={10}>
+                        <Grid container >
+                            <Grid item md={3} >
+                                <Paper sx={{ m:2 }}>1</Paper>
+                            </Grid>
+                            <Grid item md={3}>
+                                <Paper sx={{ m:2 }}>2</Paper>
+                            </Grid>
+                            <Grid item md={3}>
+                                <Paper sx={{ m:2 }}>3</Paper>
+                            </Grid>
+                            <Grid item md={3}>
+                                <Paper sx={{ m:2 }}>4</Paper>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={0} md={1}></Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
